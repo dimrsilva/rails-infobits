149228d7a35d604ca72880f99caea4d8f1af89bd
Anderson Ribeiro e Silva <dimrsilva@gmail.com>
2013-03-02 00:14:10 -0300

    Fix heroku database

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,10 @@ module Infobits
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Fix heroku bug
+    # http://www.simonecarletti.com/blog/2012/02/heroku-and-rails-3-2-assetprecompile-error/
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
