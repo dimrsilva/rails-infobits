6dd3b27aaf50958a3336c3ade643a7adc0edb8b7
Anderson Ribeiro e Silva <dimrsilva@gmail.com>
2013-03-10 15:22:08 -0300

    Validates email. Changes on menu.

--- app/models/contact/property/email.rb
+++ app/models/contact/property/email.rb
@@ -1,4 +1,5 @@
 class Contact::Property::Email < Contact::Property::Property
   default_scope :conditions => ["type = ?","Contact::Property::Email"]
-  # attr_accessible :title, :body
+
+  validates_format_of :value, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
 end
