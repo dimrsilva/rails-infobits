b1e9006687790a7c275949318cc0f7f46386b540
Anderson Ribeiro e Silva <dimrsilva@gmail.com>
2013-03-10 21:36:33 -0300

    Fix some UI (spacing on multifields, date format)

--- app/helpers/crud_helper/forms.rb
+++ app/helpers/crud_helper/forms.rb
@@ -1,6 +1,7 @@
 module CrudHelper::Forms
   class FormBuilder < ActionView::Helpers::FormBuilder
-    def date_field
+    def date_field attribute
+      text_field attribute, :type => :date
 
     # <div class="input-append date" id="dp3" data-date="12-02-2012" data-date-format="dd-mm-yyyy">
     #   <input class="span2" size="16" type="text" value="12-02-2012">
