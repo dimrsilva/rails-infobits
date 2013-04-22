0887dddada0fbd9e252be196bf6b7d59bbccbfd2
Anderson Ribeiro e Silva <dimrsilva@gmail.com>
2013-04-22 20:34:20 -0300

    Remove definition of default_scope, there is no need to it

--- app/models/contact/property/phone.rb
+++ app/models/contact/property/phone.rb
@@ -1,4 +1,3 @@
 class Contact::Property::Phone < Contact::Property::Property
-  default_scope :conditions => ["type = ?","Contact::Property::Phone"]
   # attr_accessible :title, :body
 end
--- app/models/projects/status.rb
+++ app/models/projects/status.rb
@@ -1,3 +1,2 @@
 class Projects::Status < Domain
-  default_scope :conditions => ["type = ?","Projects::Status"]
 end
