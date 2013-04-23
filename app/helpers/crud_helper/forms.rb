7fa900bcb3efcedc92d04a1518c580ef971cfef6
Anderson Ribeiro e Silva <dimrsilva@gmail.com>
2013-04-23 01:27:47 -0300

    Fix size of combobox of status

--- app/helpers/crud_helper/forms.rb
+++ app/helpers/crud_helper/forms.rb
@@ -5,7 +5,7 @@ module CrudHelper::Forms
     end
 
     def domain_field attribute, options = {}
-      collection_select attribute, options[:domain].all, :id, options[:domain].label_field
+      collection_select attribute, options[:domain].all, :id, options[:domain].label_field, {}, :class => 'input-block-level'
     end
 
     def nested_resource association
