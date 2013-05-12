9d72f0db68362fd50074070d7b5d6ace57a8de4c
Anderson Ribeiro e Silva <dimrsilva@gmail.com>
2013-05-12 14:27:55 -0300

    Allow nesting tasks, again. Format colaborators in project.

--- app/assets/javascripts/projects/projects.js
+++ app/assets/javascripts/projects/projects.js
@@ -12,20 +12,25 @@
             $(this).removeClass('icon-chevron-up').addClass('icon-chevron-down');
         });
         
-        $('.sortable.projects_tasks').sortable({
+        $('.sortable.projects_tasks').nestedSortable({
             handle: 'i.icon-move',
+            listType: 'ul',
             items: 'li',
+            toleranceElement: ' > div',
             connectWith: '.sortable.projects_tasks',
             update: function(event, updated) {
-                var arr = $(updated.item).closest('.sortable').sortable('toArray');
+                var arr = $(updated.item).closest('.sortable').nestedSortable('toArray');
                 var task_status_id = $(updated.item).closest('td').attr('id').replace(/[^\d]/g, '');
                 post_array = []
                 for(var i in arr) {
-                    post_array.push({
-                        id: arr[i].replace(/[^\d]/g, ''),
-                        index_position: i,
-                        task_status_id: task_status_id,
-                    });
+                    if(arr[i].item_id) {
+                        post_array.push({
+                            id: arr[i].item_id,
+                            parent_id: arr[i].parent_id,
+                            index_position: arr[i].left,
+                            task_status_id: task_status_id
+                        });
+                    }
                 }
                 $.ajax({
                     url: '/projetos/'+projects_project_id+'/tarefas/batch_update',
