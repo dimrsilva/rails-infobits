9130145e389d9ed3bd055cd50e0362b26d56d1ec
Anderson Ribeiro e Silva <dimrsilva@gmail.com>
2013-05-12 21:18:56 -0300

    Changes on percentage of states of projects_status,

--- spec/models/projects/task_status_spec.rb
+++ spec/models/projects/task_status_spec.rb
@@ -13,32 +13,57 @@ describe Projects::TaskStatus do
   end
   
   describe "status of work done or not" do
-    subject { task }
+    subject { FactoryGirl.create('projects/task_status', :percent => percent) }
 
-    context "with percent <= 5" do
-      let(:task) { FactoryGirl.create('projects/task_status', :percent => 5) }
+    shared_examples_for "backlogged task" do
       it { should_not be_sprinted }
       it { should_not be_work_started }
       it { should_not be_work_finished }
     end
-
-    context "with percent > 5 and < 10" do
-      let(:task) { FactoryGirl.create('projects/task_status', :percent => 6) }
+    shared_examples_for "sprinted task" do
       it { should be_sprinted }
       it { should_not be_work_started }
       it { should_not be_work_finished }
     end
-    context "with percent >= 10 and <= 90" do
-      let(:task) { FactoryGirl.create('projects/task_status', :percent => 20) }
+    shared_examples_for "started task" do
       it { should be_sprinted }
       it { should be_work_started }
       it { should_not be_work_finished }
     end
-    context "with percent > 90" do
-      let(:task) { FactoryGirl.create('projects/task_status', :percent => 91) }
+    shared_examples_for "finished task" do
       it { should be_sprinted }
       it { should be_work_started }
       it { should be_work_finished }
+    end
+
+
+    context "with percent = 0" do
+      let(:percent) { 0 }
+      it_behaves_like "backlogged task"
+    end
+    context "with percent = 5" do
+      let(:percent) { 5 }
+      it_behaves_like "backlogged task"
+    end
+    context "with percent = 6" do
+      let(:percent) { 6 }
+      it_behaves_like "sprinted task"
+    end
+    context "with percent = 10" do
+      let(:percent) { 10 }
+      it_behaves_like "sprinted task"
+    end
+    context "with percent = 11" do
+      let(:percent) { 11 }
+      it_behaves_like "started task"
+    end
+    context "with percent = 90" do
+      let(:percent) { 90 }
+      it_behaves_like "started task"
+    end
+    context "with percent = 91" do
+      let(:percent) { 91 }
+      it_behaves_like "finished task"
     end
   end
 end
